91cb75656ea65f62a69445a79becf0314a4fbdc9
ozakiryota <kb22056@gmail.com>
2019-05-10 19:34:00 +0900

    updated

--- src/initial_alignment.cpp
+++ src/initial_alignment.cpp
@@ -10,76 +10,73 @@ class ImuInitialAlignment{
 	private:
 		ros::NodeHandle nh;
 		/*subscribe*/
-		ros::Subscriber sub;
+		ros::Subscriber sub_imu;
 		/*publish*/
 		ros::Publisher pub_inipose;
 		ros::Publisher pub_bias;
 		tf::TransformBroadcaster tf_broadcaster;
 		/*const*/
-		const int num_state = 2;
-		const double timelimit = 30.0;	//[s]
+		const int size_data = 6;	//wx, wy, wx, vx, vy, vz
+		const double time_enough = 30.0;	//[s]
+		const double threshold_deflection_imu_angular = 0.03;
+		const double threshold_deflection_imu_linear = 0.2;
+		/* const double threshold_abs_imu_linear = 0.2; */
+		/* const double threshold_abs_imu_angular = 0.03; */
+		/* const double threshold_abs_odom_linear = 1.0e-3; */
+		/* const double threshold_abs_odom_angular = 1.0e-3; */
+		const int min_record_size = 10;
 		/*objects*/
 		geometry_msgs::Quaternion initial_pose;
-		Eigen::MatrixXd X;
-		Eigen::MatrixXd P;
 		std::vector<sensor_msgs::Imu> record;
 		sensor_msgs::Imu average;
-		ros::Time time_started;
 		/*flags*/
 		bool imu_is_moving = false;
 		bool initial_algnment_is_done = false;
 	public:
 		ImuInitialAlignment();
-		void Callback(const sensor_msgs::ImuConstPtr& msg);
+		void CallbackIMU(const sensor_msgs::ImuConstPtr& msg);
+		Eigen::VectorXd InputToVector(sensor_msgs::Imu imu);
+		sensor_msgs::Imu InputToImuMsg(Eigen::VectorXd Vec);
 		void ComputeAverage(void);
-		bool JudgeMoving(void);
-		void Prediction(void);
-		void Observation(void);
+		bool JudgeMovingIMU(sensor_msgs::Imu imu);
+		void ComputeInitialPose(void);
+		void InputZero(void);
 		void Publication(void);
 };
 
 ImuInitialAlignment::ImuInitialAlignment()
 {
-	sub = nh.subscribe("/imu/data", 1, &ImuInitialAlignment::Callback, this);
+	sub_imu = nh.subscribe("/imu/data", 1, &ImuInitialAlignment::CallbackIMU, this);
 	pub_inipose = nh.advertise<geometry_msgs::Quaternion>("/initial_pose", 1);
 	pub_bias = nh.advertise<sensor_msgs::Imu>("/imu/bias", 1);
-	X = Eigen::MatrixXd::Constant(num_state, 1, 0.0);
-	P = 100.0*Eigen::MatrixXd::Identity(num_state, num_state);
 }
 
-void ImuInitialAlignment::Callback(const sensor_msgs::ImuConstPtr& msg)
+void ImuInitialAlignment::CallbackIMU(const sensor_msgs::ImuConstPtr& msg)
 {
 	if(!initial_algnment_is_done){
-		double time;
-		if(record.size()<10){
-			time = 0.0;
-			time_started = ros::Time::now();
+		double duration = 0;
+		bool imu_is_moving = false;
+		if(record.size()>0){
+			duration = (msg->header.stamp - record[0].header.stamp).toSec();
+			imu_is_moving = JudgeMovingIMU(*msg);
 		}
-		else{
-			try{
-				time = (ros::Time::now() - time_started).toSec();
-			}
-			catch(std::runtime_error& ex) {
-				ROS_ERROR("Exception: [%s]", ex.what());
-			}
 
-			imu_is_moving = JudgeMoving();
-		}
-		
-		if(imu_is_moving || time>timelimit){
+		if(imu_is_moving || duration>time_enough){
 			initial_algnment_is_done = true;
-			tf::Quaternion q = tf::createQuaternionFromRPY(X(0, 0), X(1, 0), 0.0);
-			quaternionTFToMsg(q, initial_pose);
-			if(time>timelimit)	std::cout << "time > " << timelimit << "[s]" << std::endl;
-			else	std::cout << "Moved at " << time << "[s]" << std::endl;
-			std::cout << "initial pose = " << std::endl << X << std::endl;
-			std::cout << "bias = " << std::endl << average.angular_velocity.x << std::endl << average.angular_velocity.y << std::endl << average.angular_velocity.z << std::endl;
+			std::cout << "record.size() = " << record.size() << std::endl;
+			if(duration>time_enough)	std::cout << "Duration is now enough!(" << duration << " [s])" << std::endl;
+			if(record.size()<min_record_size){
+				std::cout << ">> Initial alignment is FAILEiD" << std::endl;
+				InputZero();
+			}
+			else{
+				std::cout << ">> Initial alignment is DONE!" << std::endl;
+				ComputeInitialPose();
+			}
 		}
 		else{
 			record.push_back(*msg);
 			ComputeAverage();
-			Prediction();
-			Observation();
 		}
 	}
 	else	Publication();
@@ -87,103 +84,80 @@ void ImuInitialAlignment::Callback(const sensor_msgs::ImuConstPtr& msg)
 
 void ImuInitialAlignment::ComputeAverage(void)
 {
-	average.angular_velocity.x = 0.0;
-	average.angular_velocity.y = 0.0;
-	average.angular_velocity.z = 0.0;
-	average.linear_acceleration.x = 0.0;
-	average.linear_acceleration.y = 0.0;
-	average.linear_acceleration.z = 0.0;
+	Eigen::VectorXd Ave = Eigen::VectorXd::Zero(size_data);
 	
-	for(size_t i=0;i<record.size();i++){
-		average.angular_velocity.x += record[i].angular_velocity.x/(double)record.size();
-		average.angular_velocity.y += record[i].angular_velocity.y/(double)record.size();
-		average.angular_velocity.z += record[i].angular_velocity.z/(double)record.size();
-		average.linear_acceleration.x += record[i].linear_acceleration.x/(double)record.size();
-		average.linear_acceleration.y += record[i].linear_acceleration.y/(double)record.size();
-		average.linear_acceleration.z += record[i].linear_acceleration.z/(double)record.size();
-	}
+	for(size_t i=0;i<record.size();i++)	Ave += InputToVector(record[i]);
+	Ave /= (double)record.size();
+
+	average = InputToImuMsg(Ave);
 }
 
-bool ImuInitialAlignment::JudgeMoving(void)
+bool ImuInitialAlignment::JudgeMovingIMU(sensor_msgs::Imu imu)
 {
-	// const float threshold_w = 0.03;
-	// const float threshold_a = 0.20;
-	const float threshold_w = 0.05;
-	const float threshold_a = 0.5;
-	if(fabs(record[record.size()-1].angular_velocity.x - average.angular_velocity.x)>threshold_w){
-		std::cout << "Moved-wx: " << record[record.size()-1].angular_velocity.x - average.angular_velocity.x << std::endl;
-		return true;
+	Eigen::VectorXd Ave = InputToVector(average);
+	Eigen::VectorXd New = InputToVector(imu);
+
+	for(int i=0;i<3;i++){
+		double deflection = fabs(Ave(i) - New(i));
+		if(deflection > threshold_deflection_imu_angular){
+			std::cout << "Started moving!(imu_angular) index#: " << i << std::endl;
+			return true;
+		}
 	}
-	if(fabs(record[record.size()-1].angular_velocity.y - average.angular_velocity.y)>threshold_w){
-		std::cout << "Moved-wy: " << record[record.size()-1].angular_velocity.y - average.angular_velocity.y << std::endl;
-		return true;
+	for(int i=3;i<6;i++){
+		double deflection = fabs(Ave(i) - New(i));
+		if(deflection > threshold_deflection_imu_linear){
+			std::cout << "Started moving!(imu_linear) index#: " << i << std::endl;
+			return true;
+		}
 	}
-	if(fabs(record[record.size()-1].angular_velocity.z - average.angular_velocity.z)>threshold_w){
-		std::cout << "Moved-wz: " << record[record.size()-1].angular_velocity.z - average.angular_velocity.z << std::endl;
-		return true;
-	}
-	if(fabs(record[record.size()-1].linear_acceleration.x - average.linear_acceleration.x)>threshold_a){
-		std::cout << "Moved-ax: " << record[record.size()-1].linear_acceleration.x - average.linear_acceleration.x << std::endl;
-		return true;
-	}
-	if(fabs(record[record.size()-1].linear_acceleration.y - average.linear_acceleration.y)>threshold_a){
-		std::cout << "Moved-ay: " << record[record.size()-1].linear_acceleration.y - average.linear_acceleration.y << std::endl;
-		return true;
-	}
-	if(fabs(record[record.size()-1].linear_acceleration.z - average.linear_acceleration.z)>threshold_a){
-		std::cout << "Moved-az: " << record[record.size()-1].linear_acceleration.z - average.linear_acceleration.z << std::endl;
-		return true;
-	}
+
 	return false;
 }
 
-void ImuInitialAlignment::Prediction(void)
+Eigen::VectorXd ImuInitialAlignment::InputToVector(sensor_msgs::Imu imu)
 {
-	Eigen::MatrixXd A = Eigen::MatrixXd::Identity(num_state, num_state);
-	Eigen::MatrixXd F(num_state, 1);
-	Eigen::MatrixXd jF = Eigen::MatrixXd::Identity(num_state, num_state);
-	const double sigma = 1.0e-1;
-	Eigen::MatrixXd Q = sigma*Eigen::MatrixXd::Identity(num_state, num_state);
-
-	F = A*X;
-	X = F;
-	P = jF*P*jF.transpose() + Q;
-	// std::cout << "X_pre = " << std::endl << X << std::endl;
-	// std::cout << "P_pre = " << std::endl << P << std::endl;
+	Eigen::VectorXd Vec(size_data);
+	Vec <<	imu.angular_velocity.x,
+			imu.angular_velocity.y,
+			imu.angular_velocity.z,
+			imu.linear_acceleration.x,
+			imu.linear_acceleration.y,
+			imu.linear_acceleration.z;
+	return Vec;
 }
-void ImuInitialAlignment::Observation(void)
-{
-	const int num_obs = 2;
 
+sensor_msgs::Imu ImuInitialAlignment::InputToImuMsg(Eigen::VectorXd Vec)
+{
+	sensor_msgs::Imu imu;
+	imu.angular_velocity.x = Vec(0);
+	imu.angular_velocity.y = Vec(1);
+	imu.angular_velocity.z = Vec(2);
+	imu.linear_acceleration.x = Vec(3);
+	imu.linear_acceleration.y = Vec(4);
+	imu.linear_acceleration.z = Vec(5);
+	return imu;
+}
+
+void ImuInitialAlignment::ComputeInitialPose(void)
+{
 	double ax = average.linear_acceleration.x;
 	double ay = average.linear_acceleration.y;
 	double az = average.linear_acceleration.z;
-	double roll = X(0, 0);
-	double pitch = X(1, 0);
-	
-	Eigen::MatrixXd Z(num_obs, 1);
-	Z <<	atan2(ay, az),
-			atan2(-ax, sqrt(ay*ay + az*az));
-	Eigen::MatrixXd H(num_obs, num_state);
-	H <<	1,	0,
-	  		0,	1;
-	Eigen::MatrixXd jH(num_obs, num_state);
-	jH <<	1,	0,
-			0,	1;
-	const double sigma = 1.0e-1;
-	Eigen::MatrixXd R = sigma*Eigen::MatrixXd::Identity(num_obs, num_obs);
-	Eigen::MatrixXd Y(num_obs, 1);
-	Eigen::MatrixXd S(num_obs, num_obs);
-	Eigen::MatrixXd K(num_state, num_obs);
-	Eigen::MatrixXd I = Eigen::MatrixXd::Identity(num_state, num_state);
+	double r = atan2(ay, az);
+	double p = atan2(-ax, sqrt(ay*ay + az*az));
+	quaternionTFToMsg(tf::createQuaternionFromRPY(r, p, 0), initial_pose);
 
-	Y = Z - H*X;
-	S = jH*P*jH.transpose() + R;
-	K = P*jH.transpose()*S.inverse();
-	X = X + K*Y;
-	P = (I - K*jH)*P;
+	std::cout << "inittial (roll, pitch) = (" << r/M_PI*180.0 << ", " << p/M_PI*180.0 << ")[deg]" << std::endl;
+}
 
-	// std::cout << "K*Y = " << std::endl << K*Y << std::endl;
+void ImuInitialAlignment::InputZero(void)
+{
+	average = InputToImuMsg(Eigen::VectorXd::Zero(size_data));
+	initial_pose.x = 0.0;
+	initial_pose.y = 0.0;
+	initial_pose.z = 0.0;
+	initial_pose.w = 1.0;
 }
 
 void ImuInitialAlignment::Publication(void)
@@ -207,6 +181,7 @@ int main(int argc, char**argv)
 {
 	ros::init(argc, argv, "imu_initial_alignment");
 
+	std::cout << "Initial Alignment START!" << std::endl;
 	ImuInitialAlignment imu_initial_alignment;
 
 	ros::spin();
