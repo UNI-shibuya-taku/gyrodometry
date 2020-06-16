566e2249491ce94d707a4a7e2bea104e1305bfc3
ozakiryota <kb22056@gmail.com>
2020-06-17 06:12:28 +0900

    updated

--- src/initial_alignment.cpp
+++ src/initial_alignment.cpp
@@ -2,199 +2,202 @@
 #include <Eigen/Core>
 #include <Eigen/LU>
 #include <sensor_msgs/Imu.h>
-#include <geometry_msgs/Quaternion.h>
+#include <geometry_msgs/QuaternionStamped.h>
 #include <tf/tf.h>
 #include <tf/transform_broadcaster.h>
 
-class ImuInitialAlignment{
+class InitialAlignment{
 	private:
 		/*node hangle*/
-		ros::NodeHandle nh;
-		ros::NodeHandle nhPrivate;
+		ros::NodeHandle _nh;
+		ros::NodeHandle _nhPrivate;
 		/*subscribe*/
-		ros::Subscriber sub_imu;
+		ros::Subscriber _sub_imu;
 		/*publish*/
-		ros::Publisher pub_inipose;
-		ros::Publisher pub_bias;
+		ros::Publisher _pub_quat;
+		ros::Publisher _pub_bias;
 		tf::TransformBroadcaster tf_broadcaster;
 		/*const*/
-		const int size_data = 6;	//wx, wy, wx, vx, vy, vz
-		const double time_enough = 60.0;	//[s]
-		const double threshold_deflection_imu_angular = 0.03;
-		const double threshold_deflection_imu_linear = 0.2;
-		/* const double threshold_abs_imu_linear = 0.2; */
-		/* const double threshold_abs_imu_angular = 0.03; */
-		/* const double threshold_abs_odom_linear = 1.0e-3; */
-		/* const double threshold_abs_odom_angular = 1.0e-3; */
-		const int min_record_size = 20;
-		/*objects*/
-		geometry_msgs::Quaternion initial_orientation;
-		std::vector<sensor_msgs::Imu> record;
-		sensor_msgs::Imu average;
-		/*flags*/
-		bool imu_is_moving = false;
-		bool initial_algnment_is_done = false;
-		bool initial_orientation_is_0001 = false;
+		const int _data_size = 6;	//ax, ay, az, wx, wy, wz
+		/*msg*/
+		geometry_msgs::QuaternionStamped _ini_ori;
+		sensor_msgs::Imu _bias;
+		/*time*/
+		ros::Time _start_stamp;
+		/*vector*/
+		Eigen::VectorXd _sum;
+		Eigen::VectorXd _ave;
+		/*object*/
+		int _record_size = 0;
+		/*flag*/
+		bool _is_done = false;
+		bool _is_fail = false;
+		/*parameter*/
+		bool _ini_ori_is_0001;
+		std::string _frame_id;
+		std::string _child_frame_id;
+		int _min_record_size;
+		double _max_duration;
+		double _th_linear_deflection;
+		double _th_angle_deflection;
 	public:
-		ImuInitialAlignment();
-		void CallbackIMU(const sensor_msgs::ImuConstPtr& msg);
-		Eigen::VectorXd InputToVector(sensor_msgs::Imu imu);
-		sensor_msgs::Imu InputToImuMsg(Eigen::VectorXd Vec);
-		void ComputeAverage(void);
-		bool JudgeMovingIMU(sensor_msgs::Imu imu);
-		void ComputeInitialPose(void);
-		void InputZero(void);
-		void Publication(void);
+		InitialAlignment();
+		void callbackIMU(const sensor_msgs::ImuConstPtr& msg);
+		bool isMoving(const Eigen::VectorXd& data);
+		void setMsg(std_msgs::Header imu_header);
+		void publication(void);
 };
 
-ImuInitialAlignment::ImuInitialAlignment()
-	:nhPrivate("~")
+InitialAlignment::InitialAlignment()
+	:_nhPrivate("~")
 {
-	sub_imu = nh.subscribe("/imu/data", 1, &ImuInitialAlignment::CallbackIMU, this);
-	pub_inipose = nh.advertise<geometry_msgs::Quaternion>("/initial_orientation", 1);
-	pub_bias = nh.advertise<sensor_msgs::Imu>("/imu/bias", 1);
-
-	nhPrivate.param("initial_orientation_is_0001", initial_orientation_is_0001, false);
-	if(initial_orientation_is_0001)	std::cout << ">> Initial orientation won't be estimated: (0, 0, 0, 1)" << std::endl;
-	else	std::cout << ">> Initial orientation will be estimated" << std::endl;
+	std::cout << "IMU Initial Alignment: START" << std::endl;
+	/*parameter*/
+	_nhPrivate.param("_ini_ori_is_0001", _ini_ori_is_0001, false);
+	std::cout << "_ini_ori_is_0001" << (bool)_ini_ori_is_0001 << std::endl;
+	_nhPrivate.param("_frame_id", _frame_id, std::string("/odom"));
+	std::cout << "_frame_id" << _frame_id << std::endl;
+	_nhPrivate.param("_child_frame_id", _child_frame_id, std::string("/initial_orientation"));
+	std::cout << "_child_frame_id" << _child_frame_id << std::endl;
+	_nhPrivate.param("_min_record_size", _min_record_size, 100);
+	std::cout << "_min_record_size" << _min_record_size << std::endl;
+	_nhPrivate.param("_max_duration", _max_duration, 60.0);
+	std::cout << "_max_duration" << _max_duration << std::endl;
+	_nhPrivate.param("_th_linear_deflection", _th_linear_deflection, 0.03);
+	std::cout << "_th_linear_deflection" << _th_linear_deflection << std::endl;
+	_nhPrivate.param("_th_angle_deflection", _th_angle_deflection, 0.2);
+	std::cout << "_th_angle_deflection" << _th_angle_deflection << std::endl;
+	/*subscriber*/
+	_sub_imu = _nh.subscribe("/imu/data", 1, &InitialAlignment::callbackIMU, this);
+	/*publisher*/
+	_pub_quat = _nh.advertise<geometry_msgs::Quaternion>("/initial_orientation", 1);
+	_pub_bias = _nh.advertise<sensor_msgs::Imu>("/imu/bias", 1);
+	/*initialize*/
+	_sum = Eigen::VectorXd::Zero(_data_size);
 }
 
-void ImuInitialAlignment::CallbackIMU(const sensor_msgs::ImuConstPtr& msg)
+void InitialAlignment::callbackIMU(const sensor_msgs::ImuConstPtr& msg)
 {
-	if(!initial_algnment_is_done){
-		double duration = 0;
-		bool imu_is_moving = false;
-		if(record.size()>0){
-			duration = (msg->header.stamp - record[0].header.stamp).toSec();
-			imu_is_moving = JudgeMovingIMU(*msg);
-		}
+	if(_is_done)	publication();
+	if(_is_fail)	return;
 
-		if(imu_is_moving || duration>time_enough){
-			initial_algnment_is_done = true;
-			std::cout << "record.size() = " << record.size() << std::endl;
-			if(duration>time_enough)	std::cout << "Duration is now enough!(" << duration << " [s])" << std::endl;
-			if(record.size()<min_record_size){
-				std::cout << ">> Initial alignment is FAILEiD" << std::endl;
-				InputZero();
+	/*record*/
+	Eigen::VectorXd data(_data_size);
+	data <<
+		msg->linear_acceleration.x,
+		msg->linear_acceleration.y,
+		msg->linear_acceleration.z,
+		msg->angular_velocity.x,
+		msg->angular_velocity.y,
+		msg->angular_velocity.z;
+
+	if(_record_size == 0){
+		_start_stamp = msg->header.stamp;
+	}
+	else{
+		if(isMoving(data)){
+			if(_record_size > _min_record_size){
+				_is_done = true;
+				setMsg(msg->header);
+				publication();
 			}
 			else{
-				std::cout << ">> Initial alignment is DONE!" << std::endl;
-				ComputeInitialPose();
+				_is_fail = true;
+				std::cout << "IMU Initial Alignment: FAIL" << std::endl;
+			}
+			return;
+		}
+	}
+
+	/*update*/
+	_sum += data;
+	++_record_size;
+	_ave = _sum/(double)_record_size;
+	/*dropout*/
+	double duration;
+	try{
+		duration = (msg->header.stamp - _start_stamp).toSec();
+		if(duration > _max_duration){
+			_is_done = true;
+			setMsg(msg->header);
+			publication();
+		}
+	}
+	catch(std::runtime_error& ex){
+		ROS_ERROR("Exception: [%s]", ex.what());
+	}
+}
+
+bool InitialAlignment::isMoving(const Eigen::VectorXd& data)
+{
+	Eigen::VectorXd deflection = _ave - data;
+	for(size_t i=0;i<deflection.size();++i){
+		if(i < 3){
+			if(abs(deflection(i)) > _th_linear_deflection){
+				std::cout << "Moving: " << i << std::endl;
+				return true;
 			}
 		}
 		else{
-			record.push_back(*msg);
-			ComputeAverage();
+			if(abs(deflection(i)) > _th_angle_deflection){
+				std::cout << "Moving: " << i << std::endl;
+				return true;
+			}
 		}
 	}
-	else	Publication();
-}
-
-void ImuInitialAlignment::ComputeAverage(void)
-{
-	Eigen::VectorXd Ave = Eigen::VectorXd::Zero(size_data);
-	
-	for(size_t i=0;i<record.size();i++)	Ave += InputToVector(record[i]);
-	Ave /= (double)record.size();
-
-	average = InputToImuMsg(Ave);
-}
-
-bool ImuInitialAlignment::JudgeMovingIMU(sensor_msgs::Imu imu)
-{
-	Eigen::VectorXd Ave = InputToVector(average);
-	Eigen::VectorXd New = InputToVector(imu);
-
-	for(int i=0;i<3;i++){
-		double deflection = fabs(Ave(i) - New(i));
-		if(deflection > threshold_deflection_imu_angular){
-			std::cout << "Started moving!(imu_angular) index#: " << i << std::endl;
-			return true;
-		}
-	}
-	for(int i=3;i<6;i++){
-		double deflection = fabs(Ave(i) - New(i));
-		if(deflection > threshold_deflection_imu_linear){
-			std::cout << "Started moving!(imu_linear) index#: " << i << std::endl;
-			return true;
-		}
-	}
-
 	return false;
 }
 
-Eigen::VectorXd ImuInitialAlignment::InputToVector(sensor_msgs::Imu imu)
+void InitialAlignment::setMsg(std_msgs::Header imu_header)
 {
-	Eigen::VectorXd Vec(size_data);
-	Vec <<	imu.angular_velocity.x,
-			imu.angular_velocity.y,
-			imu.angular_velocity.z,
-			imu.linear_acceleration.x,
-			imu.linear_acceleration.y,
-			imu.linear_acceleration.z;
-	return Vec;
-}
-
-sensor_msgs::Imu ImuInitialAlignment::InputToImuMsg(Eigen::VectorXd Vec)
-{
-	sensor_msgs::Imu imu;
-	imu.angular_velocity.x = Vec(0);
-	imu.angular_velocity.y = Vec(1);
-	imu.angular_velocity.z = Vec(2);
-	imu.linear_acceleration.x = Vec(3);
-	imu.linear_acceleration.y = Vec(4);
-	imu.linear_acceleration.z = Vec(5);
-	return imu;
-}
-
-void ImuInitialAlignment::ComputeInitialPose(void)
-{
-	double ax = average.linear_acceleration.x;
-	double ay = average.linear_acceleration.y;
-	double az = average.linear_acceleration.z;
-	double r = atan2(ay, az);
-	double p = atan2(-ax, sqrt(ay*ay + az*az));
-	if(initial_orientation_is_0001){
-		r = 0.0;
-		p = 0.0;
+	/*bias*/
+	_bias.header = imu_header;
+	_bias.linear_acceleration.x = _ave(0);
+	_bias.linear_acceleration.y = _ave(1);
+	_bias.linear_acceleration.z = _ave(2);
+	_bias.angular_velocity.x = _ave(3);
+	_bias.angular_velocity.y = _ave(4);
+	_bias.angular_velocity.z = _ave(5);
+	/*initial orientation*/
+	_ini_ori.header.frame_id = _frame_id;
+	_ini_ori.header.stamp = imu_header.stamp;
+	if(_ini_ori_is_0001){
+		_ini_ori.quaternion.x = 0.0;
+		_ini_ori.quaternion.y = 0.0;
+		_ini_ori.quaternion.z = 0.0;
+		_ini_ori.quaternion.w = 1.0;
 	}
-	quaternionTFToMsg(tf::createQuaternionFromRPY(r, p, 0.0), initial_orientation);
-
-	std::cout << "inittial (roll, pitch) = (" << r/M_PI*180.0 << ", " << p/M_PI*180.0 << ")[deg]" << std::endl;
+	else{
+		double r = atan2(_ave(1), _ave(2));
+		double p = atan2(_ave(0), sqrt(_ave(1)*_ave(1) + _ave(2)*_ave(2)));
+		tf::Quaternion q = tf::createQuaternionFromRPY(r, p, 0.0);
+		quaternionTFToMsg(q, _ini_ori.quaternion);
+	}
+	std::cout << "IMU Initial Alignment: DONE" << std::endl;
 }
 
-void ImuInitialAlignment::InputZero(void)
-{
-	average = InputToImuMsg(Eigen::VectorXd::Zero(size_data));
-	initial_orientation.x = 0.0;
-	initial_orientation.y = 0.0;
-	initial_orientation.z = 0.0;
-	initial_orientation.w = 1.0;
-}
-
-void ImuInitialAlignment::Publication(void)
+void InitialAlignment::publication(void)
 {
 	/*publish*/
-	pub_inipose.publish(initial_orientation);
-	pub_bias.publish(average);
+	_pub_quat.publish(_ini_ori);
+	_pub_bias.publish(_bias);
 	/*tf broadcast*/
     geometry_msgs::TransformStamped transform;
 	transform.header.stamp = ros::Time::now();
-	transform.header.frame_id = "/odom";
-	transform.child_frame_id = "/initial_orientation";
+	transform.header.frame_id = _frame_id;
+	transform.child_frame_id = _child_frame_id;
 	transform.transform.translation.x = 0.0;
 	transform.transform.translation.y = 0.0;
 	transform.transform.translation.z = 0.0;
-	transform.transform.rotation = initial_orientation;
+	transform.transform.rotation = _ini_ori.quaternion;
 	tf_broadcaster.sendTransform(transform);
 }
 
 int main(int argc, char**argv)
 {
-	ros::init(argc, argv, "imu_initial_alignment");
+	ros::init(argc, argv, "initial_alignment");
 
-	std::cout << "Initial Alignment START!" << std::endl;
-	ImuInitialAlignment imu_initial_alignment;
+	InitialAlignment initial_alignment;
 
 	ros::spin();
 }
---
+++ src/initial_alignment_.cpp
@@ -0,0 +1,200 @@
+#include <ros/ros.h>
+#include <Eigen/Core>
+#include <Eigen/LU>
+#include <sensor_msgs/Imu.h>
+#include <geometry_msgs/Quaternion.h>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
+
+class ImuInitialAlignment{
+	private:
+		/*node hangle*/
+		ros::NodeHandle nh;
+		ros::NodeHandle nhPrivate;
+		/*subscribe*/
+		ros::Subscriber sub_imu;
+		/*publish*/
+		ros::Publisher pub_inipose;
+		ros::Publisher pub_bias;
+		tf::TransformBroadcaster tf_broadcaster;
+		/*const*/
+		const int size_data = 6;	//wx, wy, wx, vx, vy, vz
+		const double time_enough = 60.0;	//[s]
+		const double threshold_deflection_imu_angular = 0.03;
+		const double threshold_deflection_imu_linear = 0.2;
+		/* const double threshold_abs_imu_linear = 0.2; */
+		/* const double threshold_abs_imu_angular = 0.03; */
+		/* const double threshold_abs_odom_linear = 1.0e-3; */
+		/* const double threshold_abs_odom_angular = 1.0e-3; */
+		const int min_record_size = 20;
+		/*objects*/
+		geometry_msgs::Quaternion initial_orientation;
+		std::vector<sensor_msgs::Imu> record;
+		sensor_msgs::Imu average;
+		/*flags*/
+		bool imu_is_moving = false;
+		bool initial_algnment_is_done = false;
+		bool initial_orientation_is_0001 = false;
+	public:
+		ImuInitialAlignment();
+		void CallbackIMU(const sensor_msgs::ImuConstPtr& msg);
+		Eigen::VectorXd InputToVector(sensor_msgs::Imu imu);
+		sensor_msgs::Imu InputToImuMsg(Eigen::VectorXd Vec);
+		void ComputeAverage(void);
+		bool JudgeMovingIMU(sensor_msgs::Imu imu);
+		void ComputeInitialPose(void);
+		void InputZero(void);
+		void Publication(void);
+};
+
+ImuInitialAlignment::ImuInitialAlignment()
+	:nhPrivate("~")
+{
+	sub_imu = nh.subscribe("/imu/data", 1, &ImuInitialAlignment::CallbackIMU, this);
+	pub_inipose = nh.advertise<geometry_msgs::Quaternion>("/initial_orientation", 1);
+	pub_bias = nh.advertise<sensor_msgs::Imu>("/imu/bias", 1);
+
+	nhPrivate.param("initial_orientation_is_0001", initial_orientation_is_0001, false);
+	if(initial_orientation_is_0001)	std::cout << ">> Initial orientation won't be estimated: (0, 0, 0, 1)" << std::endl;
+	else	std::cout << ">> Initial orientation will be estimated" << std::endl;
+}
+
+void ImuInitialAlignment::CallbackIMU(const sensor_msgs::ImuConstPtr& msg)
+{
+	if(!initial_algnment_is_done){
+		double duration = 0;
+		bool imu_is_moving = false;
+		if(record.size()>0){
+			duration = (msg->header.stamp - record[0].header.stamp).toSec();
+			imu_is_moving = JudgeMovingIMU(*msg);
+		}
+
+		if(imu_is_moving || duration>time_enough){
+			initial_algnment_is_done = true;
+			std::cout << "record.size() = " << record.size() << std::endl;
+			if(duration>time_enough)	std::cout << "Duration is now enough!(" << duration << " [s])" << std::endl;
+			if(record.size()<min_record_size){
+				std::cout << ">> Initial alignment is FAILEiD" << std::endl;
+				InputZero();
+			}
+			else{
+				std::cout << ">> Initial alignment is DONE!" << std::endl;
+				ComputeInitialPose();
+			}
+		}
+		else{
+			record.push_back(*msg);
+			ComputeAverage();
+		}
+	}
+	else	Publication();
+}
+
+void ImuInitialAlignment::ComputeAverage(void)
+{
+	Eigen::VectorXd Ave = Eigen::VectorXd::Zero(size_data);
+	
+	for(size_t i=0;i<record.size();i++)	Ave += InputToVector(record[i]);
+	Ave /= (double)record.size();
+
+	average = InputToImuMsg(Ave);
+}
+
+bool ImuInitialAlignment::JudgeMovingIMU(sensor_msgs::Imu imu)
+{
+	Eigen::VectorXd Ave = InputToVector(average);
+	Eigen::VectorXd New = InputToVector(imu);
+
+	for(int i=0;i<3;i++){
+		double deflection = fabs(Ave(i) - New(i));
+		if(deflection > threshold_deflection_imu_angular){
+			std::cout << "Started moving!(imu_angular) index#: " << i << std::endl;
+			return true;
+		}
+	}
+	for(int i=3;i<6;i++){
+		double deflection = fabs(Ave(i) - New(i));
+		if(deflection > threshold_deflection_imu_linear){
+			std::cout << "Started moving!(imu_linear) index#: " << i << std::endl;
+			return true;
+		}
+	}
+
+	return false;
+}
+
+Eigen::VectorXd ImuInitialAlignment::InputToVector(sensor_msgs::Imu imu)
+{
+	Eigen::VectorXd Vec(size_data);
+	Vec <<	imu.angular_velocity.x,
+			imu.angular_velocity.y,
+			imu.angular_velocity.z,
+			imu.linear_acceleration.x,
+			imu.linear_acceleration.y,
+			imu.linear_acceleration.z;
+	return Vec;
+}
+
+sensor_msgs::Imu ImuInitialAlignment::InputToImuMsg(Eigen::VectorXd Vec)
+{
+	sensor_msgs::Imu imu;
+	imu.angular_velocity.x = Vec(0);
+	imu.angular_velocity.y = Vec(1);
+	imu.angular_velocity.z = Vec(2);
+	imu.linear_acceleration.x = Vec(3);
+	imu.linear_acceleration.y = Vec(4);
+	imu.linear_acceleration.z = Vec(5);
+	return imu;
+}
+
+void ImuInitialAlignment::ComputeInitialPose(void)
+{
+	double ax = average.linear_acceleration.x;
+	double ay = average.linear_acceleration.y;
+	double az = average.linear_acceleration.z;
+	double r = atan2(ay, az);
+	double p = atan2(-ax, sqrt(ay*ay + az*az));
+	if(initial_orientation_is_0001){
+		r = 0.0;
+		p = 0.0;
+	}
+	quaternionTFToMsg(tf::createQuaternionFromRPY(r, p, 0.0), initial_orientation);
+
+	std::cout << "inittial (roll, pitch) = (" << r/M_PI*180.0 << ", " << p/M_PI*180.0 << ")[deg]" << std::endl;
+}
+
+void ImuInitialAlignment::InputZero(void)
+{
+	average = InputToImuMsg(Eigen::VectorXd::Zero(size_data));
+	initial_orientation.x = 0.0;
+	initial_orientation.y = 0.0;
+	initial_orientation.z = 0.0;
+	initial_orientation.w = 1.0;
+}
+
+void ImuInitialAlignment::Publication(void)
+{
+	/*publish*/
+	pub_inipose.publish(initial_orientation);
+	pub_bias.publish(average);
+	/*tf broadcast*/
+    geometry_msgs::TransformStamped transform;
+	transform.header.stamp = ros::Time::now();
+	transform.header.frame_id = "/odom";
+	transform.child_frame_id = "/initial_orientation";
+	transform.transform.translation.x = 0.0;
+	transform.transform.translation.y = 0.0;
+	transform.transform.translation.z = 0.0;
+	transform.transform.rotation = initial_orientation;
+	tf_broadcaster.sendTransform(transform);
+}
+
+int main(int argc, char**argv)
+{
+	ros::init(argc, argv, "imu_initial_alignment");
+
+	std::cout << "Initial Alignment START!" << std::endl;
+	ImuInitialAlignment imu_initial_alignment;
+
+	ros::spin();
+}
